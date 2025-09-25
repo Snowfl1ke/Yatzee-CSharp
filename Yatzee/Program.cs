cb5de311023983f524019da41e9df4b10896357a
Snowfl1ke <154212875+Snowfl1ke@users.noreply.github.com>
2025-09-25 14:52:36 +0200

    Dobbelstenen rollen/verwijderen
    Let niet op me spelling ait! top.
    euhh in deze "Update" heb ik ervoor gezorgd dat je dobbel stenen kunt rollen en dat je dobbelstenen kunt verwijderen.
    ER MOET nog toegevoegd worden dat je max 3 keer kunt dobbelen voordat je die uitslag moet gebruiken voor je uitkomst, dan moet ik ook nog fixen dat je niet een niet bestaande index moet kunnen selecteren!

--- Yatzee/Program.cs
+++ Yatzee/Program.cs
@@ -1,1 +1,67 @@
-﻿
+﻿List<int> Dices = new List<int>();
+Random rnd = new Random();
+
+for (int Rondes = 1; Rondes < 14; Rondes++)
+{
+    Console.WriteLine($"Ronde {Rondes}");
+
+    for (int Beurt = 1; Beurt < 4; Beurt++)
+    {
+        while (Dices.Count < 5)
+        {
+            Dices.Add(rnd.Next(0, 7));
+        }
+
+        bool breaker = true;
+        while (breaker)
+        {
+            Console.WriteLine("Kies 1t/m5 om die dobbelsteen te vervangen \n Kies 6 om opnieuw te dobbelen");
+            Console.WriteLine($"De lijst {string.Join(", ", Dices)}");
+
+            string? playerInput = Console.ReadLine();
+
+            if (int.TryParse(playerInput, out int playerInt))
+
+            {
+                switch (playerInt)
+                {
+                    case 1:
+                        Dices.RemoveAt(0);
+                        Console.WriteLine($"De nieuwe lijst {string.Join(", ", Dices)}");
+                        break;
+                    case 2:
+                        Dices.RemoveAt(1);
+                        Console.WriteLine($"De nieuwe lijst {string.Join(", ", Dices)}");
+                        break;
+                    case 3:
+                        Dices.RemoveAt(2);
+                        Console.WriteLine($"De nieuwe lijst {string.Join(", ", Dices)}");
+                        break;
+                    case 4:
+                        Dices.RemoveAt(3);
+                        Console.WriteLine($"De nieuwe lijst {string.Join(", ", Dices)}");
+                        break;
+                    case 5:
+                        Dices.RemoveAt(4);
+                        Console.WriteLine($"De nieuwe lijst {string.Join(", ", Dices)}");
+                        break;
+                    case 6:
+                        Console.Clear();
+                        Console.WriteLine("6");
+                        breaker = false;
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Wrong input!");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Verkeerde input, alleen cijfers 1t/m6!");
+            }
+        }
+        Console.WriteLine($"Beurt {Beurt}: {string.Join(", ", Dices)}");
+    }
+    
+}
